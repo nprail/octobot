54ee50a3f2e79fbcaaf17cbbd5e5dd97b424bc17
Noah Prail <nprail@filiosoft.com>
2018-07-31 10:36:27 -0400

    improve keywords

--- skills/progress.js
+++ skills/progress.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const apiUrl = process.env.OCTO_API_URL
 module.exports = function (controller) {
   controller.hears(
-    ['^progress'],
+    ['^progress', 'progress'],
     'direct_message,direct_mention',
     async (bot, message) => {
       const progress = await axios.get(`${apiUrl}/job`, {
