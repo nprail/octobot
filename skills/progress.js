e17204dc29021f22997f5a8f94e457374dd4a23e
Noah Prail <nprail@filiosoft.com>
2018-07-31 10:57:10 -0400

    removed file

--- skills/progress.js
+++ skills/progress.js
@@ -1,25 +1,27 @@
 const axios = require('axios')
 const apiUrl = process.env.OCTO_API_URL
 module.exports = function (controller) {
+  const progressFunc = async (bot, message) => {
+    const progress = await axios.get(`${apiUrl}/job`, {
+      headers: {
+        'x-api-key': process.env.OCTO_API_KEY
+      }
+    })
+    if (progress.data.state === 'Printing') {
+      bot.reply(
+        message,
+        `Progress: ${parseFloat(progress.data.progress.completion).toFixed(2)}%`
+      )
+    } else {
+      bot.reply(message, `State: ${progress.data.state}`)
+    }
+  }
+
   controller.hears(
     ['^progress', 'progress'],
-    'direct_message,direct_mention,mention',
-    async (bot, message) => {
-      const progress = await axios.get(`${apiUrl}/job`, {
-        headers: {
-          'x-api-key': process.env.OCTO_API_KEY
-        }
-      })
-      if (progress.data.state === 'Printing') {
-        bot.reply(
-          message,
-          `Progress: ${parseFloat(progress.data.progress.completion).toFixed(
-            2
-          )}%`
-        )
-      } else {
-        bot.reply(message, `State: ${progress.data.state}`)
-      }
-    }
+    'direct_message,direct_mention',
+    progressFunc
   )
+
+  controller.hears(['^progress', 'progress'], 'mention', progressFunc)
 }
