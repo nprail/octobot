b07886e6b5925837daf3080ce0b6878fe888cae2
Noah Prail <nprail@filiosoft.com>
2018-07-31 09:52:32 -0400

    fix storage

--- bot.js
+++ bot.js
@@ -13,16 +13,14 @@ var bot_options = {
   clientId: process.env.clientId,
   clientSecret: process.env.clientSecret,
   // debug: true,
-  scopes: ['bot'],
-  studio_token: process.env.studio_token,
-  studio_command_uri: process.env.studio_command_uri
+  scopes: ['bot']
 }
 
 // Use a mongo database if specified, otherwise store in a JSON file local to the app.
 // Mongo is automatically configured when deploying to Heroku
-if (process.env.MONGO_URI) {
+if (process.env.MONGODB_URI) {
   var mongoStorage = require('botkit-storage-mongo')({
-    mongoUri: process.env.MONGO_URI
+    mongoUri: process.env.MONGODB_URI
   })
   bot_options.storage = mongoStorage
 } else {
