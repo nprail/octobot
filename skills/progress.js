1cc19ec8c89193fd394ba3e09669821b373a82eb
Noah Prail <nprail@filiosoft.com>
2018-07-31 10:45:41 -0400

    more improvements

--- skills/progress.js
+++ skills/progress.js
@@ -3,16 +3,20 @@ const apiUrl = process.env.OCTO_API_URL
 module.exports = function (controller) {
   controller.hears(
     ['^progress', 'progress'],
-    'direct_message,direct_mention',
+    'direct_message,direct_mention,mention',
     async (bot, message) => {
       const progress = await axios.get(`${apiUrl}/job`, {
         headers: {
           'x-api-key': process.env.OCTO_API_KEY
         }
       })
-      console.log(progress.data)
       if (progress.data.state === 'Printing') {
-        bot.reply(message, `Progress: ${progress.data.progress.completion}%`)
+        bot.reply(
+          message,
+          `Progress: ${parseFloat(progress.data.progress.completion).toFixed(
+            2
+          )}%`
+        )
       } else {
         bot.reply(message, `State: ${progress.data.state}`)
       }
