7d04a0a95757269f68036d553556ea693f0f6582
Noah Prail <nprail@filiosoft.com>
2018-07-31 10:33:18 -0400

    add progress command

--- skills/progress.js
+++ skills/progress.js
@@ -1,9 +1,20 @@
+const axios = require('axios')
+const apiUrl = process.env.OCTO_API_URL
 module.exports = function (controller) {
   controller.hears(
     ['^progress'],
     'direct_message,direct_mention',
-    (bot, message) => {
-      bot.reply(message, 'I will repeat whatever you say.')
+    async (bot, message) => {
+      const progress = await axios.get(`${apiUrl}/job`, {
+        headers: {
+          'x-api-key': process.env.OCTO_API_KEY
+        }
+      })
+      if (progress.state === 'Printing') {
+        bot.reply(message, `Progress: ${progress.progress.completion}%`)
+      } else {
+        bot.reply(message, `State: ${progress.state}`)
+      }
     }
   )
 }
